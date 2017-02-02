db7c063eca5264c2df6d88e816be0f182ef3008c
Soo Yang Tan <sooyang.code@gmail.com>
2017-02-02 15:48:48 +0800

    Imported assets + implemented first section

--- config.rb
+++ config.rb
@@ -42,3 +42,10 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch = 'master'
+  deploy.build_before = true
+end
+
